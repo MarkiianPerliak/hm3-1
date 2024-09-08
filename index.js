2b7ff565fcedc0fedf94c117f6215fe27931d6d9
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2024-09-08 11:28:19 +0300

    almost-done

---
+++ index.js
@@ -0,0 +1,17 @@
+let myAge = 12;
+console.log(myAge)
+const myName = "Markiian";
+console.log(myName);
+const isStudent = true;
+console.log(isStudent)
+const myString = "Що розум людини може осягнути і в що він може повірити, те він здатний досягти"
+console.log(myString)
+const myNumber = 5;
+myNewNumber = myNumber + 10;
+console.log(myNewNumber)
+const myNull = null;
+console.log(myNull)
+console.log(prompt("what's ur name"));
+console.log(alert("Welcome"));
+console.log(confirm("Confrim?"));
+console.log(alert("Are you sure?"));
